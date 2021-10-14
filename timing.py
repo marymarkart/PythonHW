7e004fccef6f83c9077ca8ff8ce4efcd1d448a8d
Mary <mary.markart@sjsu.edu>
2021-10-14 16:49:49 -0700

    change time() to time.time() in  timing.py

--- timing.py
+++ timing.py
@@ -1,9 +1,9 @@
 import time
 
 def calculate_time(func):
-    start_time = time()
+    start_time = time.time()
     func()
-    end_time = time()
+    end_time = time.time()
     run = end_time - start_time
     print("Total time " + run)
 
