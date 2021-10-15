7c2f68d1cb5c14bdac278e45e874ae88e22bf7a1
Mary <mary.markart@sjsu.edu>
2021-10-14 20:29:08 -0700

    Added comments to triple.py

--- triple.py
+++ triple.py
@@ -1,5 +1,41 @@
 def tripler(to_triple):
+    """
+    Executes the parameter function 3 times
+
+    Parameters
+    ----------
+    to_triple:
+        The function to be executed 3 times
+
+    Returns
+    -------
+    wrapper
+
+    Examples
+    --------
+    if I have a method say_hi that prints "Hi!"
+    >>> tripler(say_hi)
+    Hi!
+    Hi!
+    Hi!
+
+    """
     def wrapper():
+        """
+        Wrapper within tripler decorator that executes the function 3 times
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        Nothing
+
+        Examples
+        --------
+        >>> tripler
+        """
         to_triple()
         to_triple()
         to_triple()
