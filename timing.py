dcdcedec45f162fab68969a2b22295bbfff1f1ad
Mary <mary.markart@sjsu.edu>
2021-10-14 20:20:11 -0700

    Added comments to timing.py

--- timing.py
+++ timing.py
@@ -1,7 +1,42 @@
 import time
 
 def calculate_time(func):
+    """
+    Calculates the time spent executing a function
+
+    Parameters
+    ----------
+    func:
+        The function to be executed that were want the execution time of
+
+    Returns
+    -------
+    wrapper
+
+    Examples
+    --------
+    >>> calculate_time(time.sleep(2))
+    2
+
+    """
     def wrapper():
+        """
+        Wrapper within calculate_time decorator that executes the function
+        and calculates the time spent executing a function
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        Nothing
+
+        Examples
+        --------
+        >>> calculate_time(time.sleep(2))
+        2
+        """
         start_time = time.time()
         func()
         end_time = time.time()
