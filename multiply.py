a70d5cc96d71b9f03c83a1672288671a0c7ba933
Mary <mary.markart@sjsu.edu>
2021-10-14 19:21:03 -0700

    add comments to multiply.py

--- multiply.py
+++ multiply.py
@@ -1,4 +1,35 @@
 def multiply_list(list):
+    """
+    Multiply all numbers from a list.
+
+    This function checks if the given input from the list is a number then
+    multiplies all values in that list.
+
+    Parameters
+    ----------
+    list: the list of values to be multiplied together
+
+    Returns
+    -------
+    int
+        The product of all values in the list.
+    False
+        The list contains an invalid item
+
+    Examples
+    --------
+    >>> list = [1, 2, 3, 7]
+    >>> multiply_list(list)
+    42
+    
+    >>> list = [3, 2, 4, 89] 
+    >>> multiply_list(list)
+    2136
+
+    >>> list = [3, "B", 4, 89] 
+    >>> multiply_list(list)
+    False
+    """
     product = 1
     for x in list:
         if isinstance(x, int):
@@ -9,15 +40,3 @@ def multiply_list(list):
             return False
     else:
         return product
-    
-y = [1, 2, 3, 7]
-
-z = multiply_list(y)
-print(z)
-
-a = [3, 2, 4, 8.9]
-b = multiply_list(a)
-print(b)
-f = [3, "d", 6]
-g = multiply_list(f)
-print(g)
